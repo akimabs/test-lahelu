a950fa4842500a119cd0aefa582c171fb54d3351
akimabs <kimuwhehe@gmail.com>
2024-09-26 14:20:54 +0700

    fix: development build

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "lahelu-web",
-      script: "npx expo export -p web && npx serve dist --single -- --port 2999",
+      script: "npx expo export -p web && npx serve dist --single -l 2999",
       instances: 1,
     },
   ],
